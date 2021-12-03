7edc960972a9ee423b1924e6b75b8c8cd3e2bd94
jianfeng <gjf610@yeah.net>
2021-12-03 10:41:26 +0800

    added inside route

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
-function route() {
-    let number = window.location.hash.substr(1)
-    let app = document.querySelector('#app')
-    number = number || 1
-    let div = document.querySelector(`#div${number}`)
-    if (!div) div = document.querySelector('#div404')
-    div.style.display = 'block'
-    if (app.children.length > 0) {
-        app.children[0].style.display = 'none'
-        document.body.appendChild(app.children[0])
-    }
+const app = document.querySelector('#app')
 
-    app.appendChild(div)
+
+const div1 = document.createElement('div')
+div1.innerHTML = '1'
+const view1 = document.createElement('div')
+div1.appendChild(view1)
+view1.style.height = '50px'
+view1.style.background = 'red'
+
+
+const div2 = document.createElement('div')
+div2.innerHTML = '2'
+const div3 = document.createElement('div')
+div3.innerHTML = '3'
+const div4 = document.createElement('div')
+div4.innerHTML = '4'
+
+const routeTable = {
+    '1': div1,
+    '2': div2,
+    '3': div3,
+    '4': div4,
+}
+const div11 = document.createElement('div')
+div11.innerHTML = '1.1'
+const div12 = document.createElement('div')
+div12.innerHTML = '1.2'
+const div13 = document.createElement('div')
+div13.innerHTML = '1.3'
+const route1Table = {
+    '1/1': div11,
+    '1/2': div12,
+    '1/3': div13,
 }
 
-route()
+
+function route(container) {
+    let number = window.location.hash.substr(1).split('/')
+    console.log(number)
+    number = number || 1
+
+    let div = routeTable[number[0].toString()]
+
+    if (!div) div = document.querySelector('#div404')
+    div.style.display = 'block'
+
+    container.innerHTML = ''
+    container.appendChild(div)
+}
+
+route(app)
 
 
 
 window.addEventListener('hashchange', () => {
     console.log('hash 变了')
-    route()
+    route(app)
 })
