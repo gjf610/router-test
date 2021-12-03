7c6f2001397e9242777f14618367d5b1a294f34f
jianfeng <gjf610@yeah.net>
2021-12-03 11:20:58 +0800

    added history

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 const app = document.querySelector('#app')
 
-
 const div1 = document.createElement('div')
 div1.innerHTML = '1'
-const view1 = document.createElement('div')
-div1.appendChild(view1)
-view1.style.height = '50px'
-view1.style.background = 'red'
-
-
 const div2 = document.createElement('div')
 div2.innerHTML = '2'
 const div3 = document.createElement('div')
@@ -17,30 +10,20 @@ const div4 = document.createElement('div')
 div4.innerHTML = '4'
 
 const routeTable = {
-    '1': div1,
-    '2': div2,
-    '3': div3,
-    '4': div4,
+    '/1': div1,
+    '/2': div2,
+    '/3': div3,
+    '/4': div4,
 }
-const div11 = document.createElement('div')
-div11.innerHTML = '1.1'
-const div12 = document.createElement('div')
-div12.innerHTML = '1.2'
-const div13 = document.createElement('div')
-div13.innerHTML = '1.3'
-const route1Table = {
-    '1/1': div11,
-    '1/2': div12,
-    '1/3': div13,
-}
+
 
 
 function route(container) {
-    let number = window.location.hash.substr(1).split('/')
+    let number = window.location.pathname
     console.log(number)
     number = number || 1
 
-    let div = routeTable[number[0].toString()]
+    let div = routeTable[number.toString()]
 
     if (!div) div = document.querySelector('#div404')
     div.style.display = 'block'
@@ -49,11 +32,24 @@ function route(container) {
     container.appendChild(div)
 }
 
+const allA = document.querySelectorAll('a.link')
+for (let a of allA) {
+    a.addEventListener("click", (e) => {
+        e.preventDefault()
+        const href = a.getAttribute('href')
+        window.history.pushState(null, `page${href}`, href)
+
+        // 通知
+        onStateChange(href)
+    })
+}
 route(app)
 
-
+function onStateChange() {
+    console.log('路由 变了')
+    route(app)
+}
 
 window.addEventListener('hashchange', () => {
-    console.log('hash 变了')
-    route(app)
+    onStateChange
 })
